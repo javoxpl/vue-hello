9ad16dbcc06cf03db28dd4b27d69b8d6deb8eed0
javoxpl <liacovone7@outlook.it>
2022-06-09 16:26:47 +0200

    exercise completed

---
+++ vue/script.js
@@ -0,0 +1,14 @@
+/*Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus: Aggiungere un’immagine presa anch’essa da un data.*/
+
+
+
+var app = new Vue ({
+    el: '#test',
+    data: {
+        titolo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        src: 'https://www.trifide.it/vita/14castori.jpg',
+        color: "bordo"
+    }
+
+});
